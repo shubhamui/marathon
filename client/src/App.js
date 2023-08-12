192be03d06e5734a85cf23f75d36dd713db0e88d
Anish-uii <anishchoudhary9285@gmail.com>
2023-08-12 18:23:20 +0530

    Added single volunteer registeration

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ export const App = () => {
           <Route path="/" exact element={<Cover />} />
           <Route path="/marathon" exact element= {localStorage.getItem("registered") ? <h1>You are registered</h1> : <Marathon />} />
           <Route path="/make-my-dp" exact element={<Dp />} />
-          <Route path="/volunteer" exact element={ window.localStorage.getItem("REGISTERED") ? <h1 id="vol-register">You are already Registered</h1> : <Volunteer /> } />
+          <Route path="/volunteer" exact element={ <Volunteer /> } />
         </Routes>
       
       </main>
--- client/src/component/menu/index.jsx
+++ client/src/component/menu/index.jsx
@@ -4,15 +4,28 @@ import AddAPhotoTwoToneIcon from '@mui/icons-material/AddAPhotoTwoTone';
 import DirectionsRunTwoToneIcon from '@mui/icons-material/DirectionsRunTwoTone';
 import VolunteerActivismTwoToneIcon from '@mui/icons-material/VolunteerActivismTwoTone';
 import SupportAgentTwoToneIcon from '@mui/icons-material/SupportAgentTwoTone';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export const Menu = ({setMenuOpen}) => {
+
+  const navigate = useNavigate()
+
+  const onVolunteerClick = () => {
+    if(!window.localStorage.getItem("REGISTERED")){
+      navigate("/volunteer")
+      
+    }else{
+      alert('Already applied !')
+    }
+    setMenuOpen(false)
+    
+  }
   return (
     <Wrapper>
         <ul>
             <li onClick={e => setMenuOpen(false)}><Link to="/make-my-dp"><AddAPhotoTwoToneIcon />Create DP</Link></li>
             <li onClick={e => setMenuOpen(false)}><Link to="/marathon"><DirectionsRunTwoToneIcon />Marathon Registration</Link></li>
-            <li onClick={e => setMenuOpen(false)}><Link to="/volunteer"><VolunteerActivismTwoToneIcon />Be a Volunteer</Link></li>
+            <li onClick={onVolunteerClick}><Link><VolunteerActivismTwoToneIcon />Be a Volunteer</Link></li>
             <li onClick={e => setMenuOpen(false)}><Link to="/support"><SupportAgentTwoToneIcon />Contact</Link></li>
         </ul>
     </Wrapper>
